83ca9d429de863a92a32b19c2639bdfc51f6c2f2
SusieHatter <susiivaldez@gmail.com>
2022-04-30 18:00:24 -0500

    Final styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,26 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["'Fira Sans'", ...defaultTheme.fontFamily.sans],
-        serif: ["'Cormorant Garamond'", ...defaultTheme.fontFamily.serif],
+        serif: ["'Marcellus'", ...defaultTheme.fontFamily.serif],
         mono: ["'Inconsolata'", ...defaultTheme.fontFamily.mono],
+      },
+
+      colors: {
+        brands: {
+          js: "#ede15b",
+          ts: "#4178e7",
+          react: "#58e3ff",
+          go: "#27aeef",
+          python: "#4495d3",
+          godot: "#3ebcc1",
+          node: "#87bc45",
+          html: "#ef9b20",
+          medicine: "#ff604e",
+          ws: "#f46a9b",
+          git: "#edbf33",
+          db: "#c37af8",
+          cli: "#976aff",
+        },
       },
     },
   },
